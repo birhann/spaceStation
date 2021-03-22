a20bb95fd6a3f7ad7f69488c849526bea36639cd
birhann <birhankarahasan@outlook.com>
2021-03-22 17:27:08 +0300

    added main window and title bar

--- components/turksatMuy.py
+++ components/turksatMuy.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDesktopWidget, QMessageBox
+from PyQt5 import QtCore
 from templates.Ui_MainGUI import Ui_MainGUI
 
 
@@ -7,6 +8,60 @@ class TurksatMuy(QMainWindow, Ui_MainGUI):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # setting position for window and hiding title bar
+        self.oldPos = self.pos()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        # title bar operations
+        self.button_max_min.clicked.connect(self.maximized_minimized)
+        self.button_close.clicked.connect(self.closeWindow)
+        self.button_minimize.clicked.connect(self.showMinimized)
+
+    def maximized_minimized(self):
+        if self.isMaximized():
+            self.showNormal()
+        else:
+            self.showMaximized()
+
+    def closeWindow(self):
+        cevap = self.confirm(
+            "Hoop", 'Programı sonlandırmak istediğinize emin misiniz')
+        if cevap:
+            sys.exit()
+        else:
+            pass
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QtCore.QPoint(event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def confirm(self, baslik="Hoop", soru="Bu işlemi yapmak istediğinizden emin misiniz?"):
+        box = QMessageBox()
+        # box.setWindowIcon(QtGui.QIcon('components/emin_misiniz.png'))
+        box.setIcon(QMessageBox.Question)
+        box.setWindowTitle(baslik)
+        box.setText(soru)
+        box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        buttonY = box.button(QMessageBox.Yes)
+        buttonY.setText('Evet')
+        buttonN = box.button(QMessageBox.No)
+        buttonN.setText('İptal')
+        box.exec_()
+        if box.clickedButton() == buttonY:
+            return True
+        elif box.clickedButton() == buttonN:
+            return False
 
 
 if __name__ == "__main__":
--- templates/Ui_MainGUI.py
+++ templates/Ui_MainGUI.py
@@ -14,11 +14,124 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainGUI(object):
     def setupUi(self, MainGUI):
         MainGUI.setObjectName("MainGUI")
-        MainGUI.resize(929, 740)
+        MainGUI.resize(788, 680)
         self.centralwidget = QtWidgets.QWidget(MainGUI)
+        self.centralwidget.setStyleSheet("margin:0px;\n"
+"padding:0px;\n"
+"")
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(7)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.title_bar_layout = QtWidgets.QHBoxLayout()
+        self.title_bar_layout.setSpacing(0)
+        self.title_bar_layout.setObjectName("title_bar_layout")
+        self.left_bar = QtWidgets.QFrame(self.centralwidget)
+        self.left_bar.setEnabled(True)
+        self.left_bar.setMinimumSize(QtCore.QSize(100, 38))
+        self.left_bar.setMaximumSize(QtCore.QSize(16777215, 38))
+        self.left_bar.setStyleSheet("QFrame{\n"
+"background:rgba(0,0,0,0.38);\n"
+"}\n"
+"QFrame{\n"
+"border-radius:none;\n"
+"}\n"
+"\n"
+"QFrame:hover{\n"
+"background:rgba(0,0,0,0.40);\n"
+"}")
+        self.left_bar.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.left_bar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.left_bar.setObjectName("left_bar")
+        self.title_bar_layout.addWidget(self.left_bar)
+        self.right_bar = QtWidgets.QFrame(self.centralwidget)
+        self.right_bar.setEnabled(True)
+        self.right_bar.setMinimumSize(QtCore.QSize(180, 38))
+        self.right_bar.setMaximumSize(QtCore.QSize(180, 38))
+        self.right_bar.setStyleSheet("QFrame{\n"
+"background:rgba(0,0,0,0.38);\n"
+"}\n"
+"QFrame{\n"
+"border-radius:none;\n"
+"}\n"
+"\n"
+"QFrame:hover{\n"
+"background:rgba(0,0,0,0.40);\n"
+"}")
+        self.right_bar.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.right_bar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.right_bar.setObjectName("right_bar")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.right_bar)
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.button_minimize = QtWidgets.QPushButton(self.right_bar)
+        self.button_minimize.setMinimumSize(QtCore.QSize(60, 38))
+        self.button_minimize.setMaximumSize(QtCore.QSize(60, 38))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.button_minimize.setFont(font)
+        self.button_minimize.setStyleSheet("QPushButton{\n"
+"background:rgba(0, 0, 0,0.15);\n"
+"color:#fff;\n"
+"\n"
+"border-radius:none;\n"
+"\n"
+"padding-top:-7px\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton:hover{\n"
+"background:#e81123;\n"
+"color:white\n"
+"}")
+        self.button_minimize.setObjectName("button_minimize")
+        self.horizontalLayout_3.addWidget(self.button_minimize)
+        self.button_max_min = QtWidgets.QPushButton(self.right_bar)
+        self.button_max_min.setMinimumSize(QtCore.QSize(60, 38))
+        self.button_max_min.setMaximumSize(QtCore.QSize(60, 38))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.button_max_min.setFont(font)
+        self.button_max_min.setStyleSheet("QPushButton{\n"
+"background:rgba(0, 0, 0,0.15);\n"
+"color:#fff;\n"
+"\n"
+"border-radius:none;\n"
+"padding-top:-3px\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton:hover{\n"
+"background:#e81123;\n"
+"color:white\n"
+"}")
+        self.button_max_min.setObjectName("button_max_min")
+        self.horizontalLayout_3.addWidget(self.button_max_min)
+        self.button_close = QtWidgets.QPushButton(self.right_bar)
+        self.button_close.setMinimumSize(QtCore.QSize(60, 38))
+        self.button_close.setMaximumSize(QtCore.QSize(60, 38))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.button_close.setFont(font)
+        self.button_close.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.button_close.setStyleSheet("QPushButton{\n"
+"background:rgba(0, 0, 0,0.15);\n"
+"color:#fff;\n"
+"\n"
+"border-radius:none;\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton:hover{\n"
+"background:#e81123;\n"
+"color:white\n"
+"}")
+        self.button_close.setObjectName("button_close")
+        self.horizontalLayout_3.addWidget(self.button_close, 0, QtCore.Qt.AlignLeft)
+        self.title_bar_layout.addWidget(self.right_bar)
+        self.verticalLayout_2.addLayout(self.title_bar_layout)
         self.main_layout = QtWidgets.QGridLayout()
         self.main_layout.setObjectName("main_layout")
         self.top_layout = QtWidgets.QHBoxLayout()
@@ -28,6 +141,9 @@ class Ui_MainGUI(object):
         self.top_layout.addLayout(self.port)
         self.data = QtWidgets.QVBoxLayout()
         self.data.setObjectName("data")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.data.addWidget(self.pushButton)
         self.top_layout.addLayout(self.data)
         self.data_transfer = QtWidgets.QVBoxLayout()
         self.data_transfer.setObjectName("data_transfer")
@@ -41,11 +157,6 @@ class Ui_MainGUI(object):
         self.main_layout.addLayout(self.bottom_layout, 2, 0, 1, 1)
         self.verticalLayout_2.addLayout(self.main_layout)
         MainGUI.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainGUI)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 929, 26))
-        self.menubar.setNativeMenuBar(False)
-        self.menubar.setObjectName("menubar")
-        MainGUI.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainGUI)
         self.statusbar.setSizeGripEnabled(True)
         self.statusbar.setObjectName("statusbar")
@@ -57,3 +168,7 @@ class Ui_MainGUI(object):
     def retranslateUi(self, MainGUI):
         _translate = QtCore.QCoreApplication.translate
         MainGUI.setWindowTitle(_translate("MainGUI", "TM MARM-99 YER ISTASYONU"))
+        self.button_minimize.setText(_translate("MainGUI", "_"))
+        self.button_max_min.setText(_translate("MainGUI", "🗖"))
+        self.button_close.setText(_translate("MainGUI", "✕"))
+        self.pushButton.setText(_translate("MainGUI", "PushButton"))
