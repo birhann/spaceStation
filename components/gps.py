b1e29308569acbbce2f73381dfdbd2cb5db9657d
Ayberk Arslan <a.arslan1499@gmail.com>
2021-04-05 22:13:09 +0300

    gps denemesi

---
+++ components/gps.py
@@ -0,0 +1,63 @@
+import sys
+import io
+import folium  # pip install folium
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWebEngineWidgets import QWebEngineView  # pip install PyQtWebEngine
+
+"""
+Folium in PyQt5
+"""
+
+
+class Koordinat():
+    x = 40.75990185278211
+    y = 29.90550476805874
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Folium in PyQt Example')
+        self.window_width, self.window_height = 1600, 1200
+        self.setMinimumSize(self.window_width, self.window_height)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        
+        anlik_x = Koordinat().x
+        anlik_y = Koordinat().y
+
+
+
+        m = folium.Map(
+            tiles='Stamen Terrain',
+            zoom_start=14,
+            location=(anlik_x,anlik_y),
+            )
+
+        # save map data to data object
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+
+        webView = QWebEngineView()
+        webView.setHtml(data.getvalue().decode())
+        layout.addWidget(webView)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 35px;
+        }
+    ''')
+
+    myApp = MyApp()
+    myApp.show()
+
+
+    try:
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
