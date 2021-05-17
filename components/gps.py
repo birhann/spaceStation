403f4275265c474867db103fcb30c76bd3111ecd
birhann <birhankarahasan@outlook.com>
2021-05-17 23:52:56 +0300

    gps simulation demo - stable

--- components/gps.py
+++ components/gps.py
@@ -1,59 +1,154 @@
 import sys
 import io
-import random
+from PyQt5 import sip
+from PyQt5.QtGui import qRgba
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QThread, pyqtSignal
+import folium
+from folium.map import Icon
 import time
-import folium  # pip install folium
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
-from PyQt5.QtWebEngineWidgets import QWebEngineView  # pip install PyQtWebEngine
+import random
 
-    
-class MyApp(QWidget):
+
+class Worker(QThread):
+    updateMap = pyqtSignal(object, object)
+    workerStatus = True
+    counter = 0
+
+    def getCoordinates(self):
+        # x = random.random()*50.0
+        # y = random.random()*50.0 40.76358185278211, 29.90650776805874
+        # koceli 40.76358185278211 29.90650776805874
+        # 38.76226363276871, 33.661376158029405
+        x, y = 38.76226363276871+random.random()*0.0015, 33.661376158029405 + \
+            random.random()*0.0015
+        return x, y
+
+    def run(self):
+        while self.counter < 8:
+            if self.workerStatus:
+                x, y = self.getCoordinates()
+                print(x, y)
+                self.updateMap.emit(x, y)
+                self.counter += 1
+                time.sleep(1)
+            else:
+                break
+
+
+class LiveMap(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Folium in PyQt Example')
-        self.window_width, self.window_height = 1600, 1200
+        self.mapStatus = False
+        self.counter = 0
+        self.defaultCoordinates = 40.76358185278211, 29.90650776805874
+        self.createWindow()
+        self.createMap(self.defaultCoordinates)
+        self.startLiveMap()
+
+    def createWindow(self):
+        self.setWindowTitle('Live Map Demo')
+        self.window_width, self.window_height = 516, 384
         self.setMinimumSize(self.window_width, self.window_height)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
 
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+    def createMap(self, coordinates):
+        mapObject = folium.Map(
+            tiles='CartoDB dark_matter',
+            zoom_start=14,
+            max_bounds=True,
+            location=(coordinates[0], coordinates[1]),
+        )
 
-        for i in range(5):
-            x = random.uniform(40.6, 40.8)
-            y= random.uniform(29.8, 30.0)
-            time.sleep(10)
-            print(x,y)
+        data = io.BytesIO()
 
-            m = folium.Map(
-                tiles='cartodbpositron',
-                zoom_start=14,
-                location=(x,y),
-                
-            )
-            folium.Marker(location=(x,y),popup='Uydu').add_to(m)
-            # save map data to data object
-            data = io.BytesIO()
-            m.save(data, close_file=False)
+        mapObject.save(data, close_file=False)
 
-        webView = QWebEngineView()
-        webView.setHtml(data.getvalue().decode())
-        layout.addWidget(webView)
-    
-            
+        self.webView = QWebEngineView()
+        self.webView.setHtml(data.getvalue().decode())
+        self.layout.addWidget(self.webView)
+
+    def startLiveMap(self):
+        if not self.mapStatus:
+            self.mapStatus = True
+            try:
+                self.thread = Worker()
+                self.thread.daemon = True
+                self.thread.updateMap.connect(self.setCoordinate)
+                self.thread.finished.connect(self.setLastInfos)
+                self.thread.start()
+            except:
+                print("VIDEO STREAMING ERRORS:", Exception)
+        else:
+            self.mapStatus = False
+            self.thread.workerStatus = False
+
+    def deleteLayout(self, cur_lay):
+        # QtGui.QLayout(cur_lay)
+
+        if cur_lay is not None:
+            while cur_lay.count():
+                item = cur_lay.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.deleteLayout(item.layout())
+            sip.delete(cur_lay)
+
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearLayout(item.layout())
+
+    def setLastInfos(self):
+        print("map is over..")
+
+    def setCoordinate(self, x, y):
+        # self.webView.deleteLater()
+        # self.deleteLayout(self.layout)
+        # self.layout = QVBoxLayout()
+        # self.setLayout(self.layout)
+        mapObject = folium.Map(
+            tiles='cartodbdark_matter',
+            zoom_start=14,
+            max_bounds=True,
+            # zoom_control=False,
+            scrollWheelZoom=False,
+            # dragging=False,
+            location=(x, y),
+        )
+
+        folium.Marker(
+            [38.76226363276871, 33.661376158029405], popup="<i>Ground Station</i>", icon=folium.Icon(color='red', icon='laptop', prefix='fa', icon_color='#FFFFFF')).add_to(mapObject)
+
+        folium.CircleMarker(
+            location=[38.76226363276871, 33.661376158029405],
+            radius=150,
+            popup='Flight Area',
+            color='#428bca',
+            fill=True,
+            fill_color='#428bca'
+        ).add_to(mapObject)
+
+        folium.Marker(
+            [x, y], popup="<i>Satellite</i>", icon=folium.Icon(icon='wifi', prefix='fa')).add_to(mapObject)
+
+        data = io.BytesIO()
+        mapObject.save(data, close_file=False)
+
+        self.webView.setHtml(data.getvalue().decode())
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyleSheet('''
-        QWidget {
-            font-size: 35px;
-        }
-    ''')
-    
-
-    myApp = MyApp()
-    myApp.show()
-
-    try:
-        sys.exit(app.exec_())
-    except SystemExit:
-        print('Closing Window...')
+    w = LiveMap()
+    w.show()
+    sys.exit(app.exec_())
